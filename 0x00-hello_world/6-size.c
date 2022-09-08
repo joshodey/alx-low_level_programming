3cdbe38f78293c5c27262f8b294a3c3cbaef655f
joshodey <joshodey@live.com>
2022-09-08 22:54:44 +0100

    level up 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	
+	printf("size of a char: %ld byte(s)\n", sizeof(a));
+	printf("size of a int: %ld byte(s)\n", sizeof(b));
+	printf("size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
+	printf("size of a float: %ld byte(s)\n", sizeof(e));
+
+	return (0);
+}
+
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("Hello World\n");
-	return(0);
+    return (0);
 }
-
