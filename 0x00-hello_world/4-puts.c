2d344553a9e6f9c05688604bf3a8f29f4c54b6da
joshodey1 <joshodey@ymail.com>
2022-09-10 10:57:19 -0700

    betty 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
@@ -11,7 +13,9 @@
  *     */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	    return (0);
+
 }
