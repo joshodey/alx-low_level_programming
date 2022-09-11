2ee0e125c1a216819501f108c96e53b4086f1b59
joshodey1 <joshodey@ymail.com>
2022-09-11 14:15:38 -0700

    5 and 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers in base 10
+ *
+ * Description: print all numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - print numbers in 10
+ *
+ * Description: print all numbers in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
