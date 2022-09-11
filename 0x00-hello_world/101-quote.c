a4e4bd4d4a920aad5d922d2844754081b23219e3
joshodey1 <joshodey@ymail.com>
2022-09-11 08:26:38 -0700

    quote messages

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Event point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	char quote;
+
+	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar(quote);
+
+	putchar('\n');
+	return (1);
 }
