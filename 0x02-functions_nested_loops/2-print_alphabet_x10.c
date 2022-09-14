976d284b287bc63231afee65ca73163c27fa649c
joshodey1 <joshodey@ymail.com>
2022-09-13 17:55:51 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ *
+ **/
+
+int print_alphabet_x10(void)
+{
+	  char t;
+	  int a;
+
+	  for (a = 1 ; a <= 10 ; a++)
+	  {
+		  for (t = 'a' ; t <= 'z' ; t++)
+	  {
+		  putchar(albt);
+	  }
+		  putchar('\n');
+	  }
+}
