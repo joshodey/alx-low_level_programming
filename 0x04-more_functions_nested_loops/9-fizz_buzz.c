473d16a5f11a1756b4092ea1b5a7c1d8b53dd816
joshodey1 <joshodey@ymail.com>
2022-09-16 02:15:37 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - fizz buzz challenge
+ *
+ * Description: prints 1 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("fizz buzz");
+		}
+		continue;
+	}
+	return (i);
+}
