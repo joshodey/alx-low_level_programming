e99c383cb82baefc00dafd4fbde68e4d554c80aa
joshodey1 <joshodey@ymail.com>
2022-09-17 06:42:00 -0700

    deadline

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - check for uppercase
+ *
+ * Description: check for uppercase letters
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' &&  c<= 'Z')
+	{
+		return (1);
+	}
+	return (0);
