37c63bfd06443c29891cfa439a4da0b3cc9f03ea
joshodey1 <joshodey@ymail.com>
2022-09-17 07:46:55 -0700

    deadline

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,13 @@ void print_most_numbers(void);
 {
 	int c;
 
-	for (c = 0 ; c <= 9 ; c++)
+	for (c = 48 ; c <= 57 ; c++)
 	{
-		if ( c == 2 || c == 4)
+		if ( c == 50 || c == 52)
 		{
 			continue;
 		}
 		putchar(c);
 	}
-	putchar('\n');
+	putchar(10);
 }
