7057a631bf0dcef71b97adac7f344284059b4461
joshodey1 <joshodey@ymail.com>
2022-09-17 07:40:06 -0700

    deadline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers: print all digits from
+ * 0 - 9
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	for (int i = 48 ; i <= 58 ; i++)
+	{
+		purchar(i);
+	}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ * among numbers
+ * Description: print most
+ * Return: 0
+ */
+void print_most_numbers(void);
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+	{
+		if ( c == 2 || c == 4)
+		{
+			continue;
+		}
+		putchar(c);
+	}
+	putchar('\n');
